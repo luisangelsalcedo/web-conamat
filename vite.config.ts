9f397bbb4594f0e28f5c959c2ec1df7fddf81008
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2024-07-30 18:54:45 -0500

    chore: Resolve url alias '@'

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { ConfigEnv, defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }: ConfigEnv) => ({
@@ -9,7 +10,14 @@ export default defineConfig(({ command }: ConfigEnv) => ({
     host: true,
   },
   plugins: [react(), svgr()],
-
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(path.join(__dirname, 'src')),
+      },
+    ],
+  },
   build: {
     outDir: 'dist',
   },
