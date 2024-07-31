ecbf4e9624b50b048d211bd4c91585388812db41
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2024-07-30 20:06:46 -0500

    feat: Define global styles and vars

---
+++ src/components/molecules/header/Header.tsx
@@ -0,0 +1,10 @@
+import './header.scss';
+
+export function Header() {
+  return (
+    <header className='header'>
+      <div className='header-top'>[LOGO] [SOCIAL-MEDIA-MENU]</div>
+      [MAIN-MENU]
+    </header>
+  );
+}
