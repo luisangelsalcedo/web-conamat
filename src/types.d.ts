2bd12b8fe9280f53cfb453970c74f33823da7fd9
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2024-07-31 05:45:19 -0500

    feat: Add Page and PageApi types

--- src/types.d.ts
+++ src/types.d.ts
@@ -18,7 +18,11 @@ export interface Acf {
   full_image: number;
   id_youttube_video: string;
 }
+export interface GUID {
+  rendered: string;
+}
 
+// * ----------------------------------------- MenuItem
 export interface MenuItem {
   id: number;
   label: string;
@@ -29,10 +33,35 @@ export interface MenuItem {
   sort: number;
   className: string;
 }
-
 export interface MenuItemApi {
   id: number;
   name: string;
   slug: string;
+  acf: Acf;
+}
+
+// * ----------------------------------------- Page
+export interface Page {
+  id: number;
+  title: string;
+  slug: string;
+  content: string;
+  excerpt: string;
+  status: string;
+  menuitem: number[];
+  sort: number;
+  className: string;
+  href: string;
+  target: string;
+}
+export interface PageApi {
+  id: number;
+  slug: string;
+  status: string;
+  title: GUID;
+  content: GUID;
+  excerpt: GUID;
+  menu_order: number;
+  menuitems: number[];
   acf: Acf;
 }
