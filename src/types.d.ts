8746092354c9a547e1a01b8903bc1349a9066be4
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2024-07-30 22:50:33 -0500

    feat: Add ItemMenu types

---
+++ src/types.d.ts
@@ -0,0 +1,38 @@
+declare global {
+  // eslint-disable-next-line no-unused-vars
+  interface Array<T> {
+    toSorted(compareFn?: (a: T, b: T) => number): T[];
+  }
+}
+
+export interface Acf {
+  href: string;
+  target: string;
+  active: boolean;
+  sort: number;
+  css_class: string;
+  publication_date: string;
+  highlight: boolean;
+  main_image: number;
+  mobile_image: number;
+  full_image: number;
+  id_youttube_video: string;
+}
+
+export interface ItemMenu {
+  id: number;
+  label: string;
+  href: string;
+  active: boolean;
+  target: string;
+  slug: string;
+  sort: number;
+  className: string;
+}
+
+export interface ItemMenuApi {
+  id: number;
+  name: string;
+  slug: string;
+  acf: Acf;
+}
