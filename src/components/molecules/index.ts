c621479be68b6490f4cb1dd66652dc3b317dcb70
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2024-07-31 07:44:26 -0500

    feat: Create slider component

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,2 +1,3 @@
 export { Header } from './header/Header';
 export { Footer } from './footer/Footer';
+export { Slider } from './slider/Slider';
---
+++ src/components/molecules/slider/Slider.tsx
@@ -0,0 +1,52 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import type { Slider } from '@/types';
+import { Link } from 'react-router-dom';
+import { Skeleton } from '@mui/material';
+import './slider.scss';
+
+interface Props {
+  sliders: Slider[];
+  isLoading: boolean;
+}
+
+export function Slider({ sliders, isLoading }: Props) {
+  return (
+    <section id='banner' className='slider'>
+      {isLoading ? (
+        <Skeleton variant='rounded' height={300} />
+      ) : (
+        <Swiper
+          className='mySwiper'
+          modules={[Autoplay, Pagination, Navigation]}
+          slidesPerView={1}
+          autoplay={{
+            delay: 4500,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: true,
+          }}
+          pagination={sliders.length > 1}
+          navigation={sliders.length > 1}
+          loop={sliders.length > 1}
+        >
+          {sliders.map(item => (
+            <SwiperSlide key={item.id}>
+              <Link to={item.href} target={item.target}>
+                <img
+                  src={item.mobileImage?.sizes.full?.source_url}
+                  width='100%'
+                  className='mobile-image'
+                />
+                <img
+                  src={item.image?.sizes.full?.source_url}
+                  width='100%'
+                  className='full-image'
+                />
+              </Link>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      )}
+    </section>
+  );
+}
