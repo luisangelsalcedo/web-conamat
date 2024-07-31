4669834739d57d3a88f5dc238df960bcc395829f
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2024-07-31 11:07:40 -0500

    feat: Implement no found page

--- src/pages/nofound-page/NofoundPage.tsx
+++ src/pages/nofound-page/NofoundPage.tsx
@@ -1,5 +1,20 @@
+import { Link } from 'react-router-dom';
+import { config } from '@/config';
+import { Isotipo } from '@/assets/svgs';
 import './nofound-page.scss';
 
 export function NofoundPage() {
-  return <div>NofoundPage</div>;
+  return (
+    <div className='nofound-page'>
+      <Isotipo className='logotipo' />
+      <h1>Página no encontrada</h1>
+      <p>
+        Parece que ha habido un error con la página que estabas buscando.
+        <br />
+        Es posible que la entrada haya sido eliminada o que la dirección no
+        exista.
+      </p>
+      <Link to={config.baseUrl}>Ir a la página de Inicio</Link>
+    </div>
+  );
 }
