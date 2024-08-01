7c85781a705e9a6a499847c8ca24012cd2164d88
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2024-07-31 19:46:12 -0500

    fix: Load blog articles by default according to the page number

--- src/store/hooks/useBlogItem.tsx
+++ src/store/hooks/useBlogItem.tsx
@@ -1,10 +1,16 @@
+import { useEffect } from 'react';
 import { useAtom, useSetAtom } from 'jotai';
 import { getBlogItemAtom, getBlogItemsAtom, blogItemAtom } from '@/store';
 
-export function useBlogItem() {
+export function useBlogItem(page: number) {
   const [blogItems] = useAtom(blogItemAtom);
   const getblogItems = useSetAtom(getBlogItemsAtom);
   const getblogItem = useSetAtom(getBlogItemAtom);
 
-  return { blogItems, getblogItems, getblogItem };
+  useEffect(() => {
+    (!blogItems.data.length || blogItems.currentPage !== page) &&
+      getblogItems(page);
+  }, [page]);
+
+  return { blogItems, getblogItem, getblogItems };
 }
