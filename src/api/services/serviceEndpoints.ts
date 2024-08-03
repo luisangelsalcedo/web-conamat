a68cf5585532e53bca37c719a79ebba7e0a7a527
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2024-08-03 07:22:07 -0500

    feat: Add api response limit as configuration parameter (default = 100)

--- src/api/services/serviceEndpoints.ts
+++ src/api/services/serviceEndpoints.ts
@@ -5,7 +5,12 @@ const base = config.apiUrl;
 interface GetEndpoint {
   (
     key: 'menuitems' | 'sliders' | 'posts' | 'pages' | 'media',
-    config?: { id?: number; slug?: string; page?: number }
+    config?: {
+      id?: number;
+      slug?: string;
+      page?: number;
+      limit?: number;
+    }
   ): string;
 }
 
@@ -13,43 +18,40 @@ const params = {
   menuitems: {
     key: 'menuitems',
     field: '_fields=id,slug,status,acf,name',
-    limit: 'per_page=100',
   },
   sliders: {
     key: 'sliders',
     field: '_fields=id,slug,status,acf,title,featured_media',
-    limit: 'per_page=100',
   },
   posts: {
     key: 'posts',
     field:
       '_fields=id,slug,status,acf,title,content,excerpt,featured_media,categories',
-    limit: 'per_page=20',
   },
   pages: {
     key: 'pages',
     field:
       '_fields=id,slug,status,acf,title,content,excerpt,menuitems,menu_order',
-    limit: 'per_page=100',
   },
   media: {
     key: 'media',
     field: '_fields=id,slug,status,acf,title,guid,media_details',
-    limit: 'per_page=100',
   },
 };
 
 export const getEndpoint: GetEndpoint = (key, config = {}): string => {
-  const { id, slug, page } = config;
+  const { id, slug, page, limit = 100 } = config;
 
   if (Object.keys(params).some(item => key === item)) {
     let endpoint = `${base}/${key}`;
 
     endpoint = id ? `${endpoint}/${id}` : endpoint;
-    endpoint = `${endpoint}?${params[key].field}&${params[key].limit}`;
+    endpoint = `${endpoint}?${params[key].field}`;
     endpoint = slug ? `${endpoint}&slug=${slug}` : endpoint;
     endpoint = page ? `${endpoint}&page=${page}` : endpoint;
+    endpoint = limit ? `${endpoint}&per_page=${limit}` : endpoint;
 
+    console.log(endpoint);
     return endpoint;
   }
   throw new Error('Endpoint no found');
