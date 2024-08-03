879b6bf8374fc1c2235a4133bd35b1017ff90452
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2024-08-02 21:35:28 -0500

    feat: Add new endpoints

--- src/api/services/serviceEndpoints.ts
+++ src/api/services/serviceEndpoints.ts
@@ -5,21 +5,27 @@ const base = config.apiUrl;
 export const endpoints = {
   blogItem: {
     GETALL: (page: number) =>
-      `${base}/posts?page=${page}&_fields=id,slug,title,content,excerpt,featured_media,status,categories,acf`,
+      `${base}/posts?page=${page}&_fields=id,slug,title,content,excerpt,featured_media,status,categories,acf&per_page=20`,
     GETBYID: (id: number) =>
       `${base}/posts/${id}?_fields=id,slug,title,content,excerpt,featured_media,status,categories,acf`,
+    GETBYSLUG: (slug: string) =>
+      `${base}/slug/${slug}?_fields=id,slug,title,content,excerpt,featured_media,status,categories,acf`,
   },
   menuItems: {
-    GETALL: `${base}/menuitems?_fields=id,name,slug,acf`,
+    GETALL: `${base}/menuitems?_fields=id,name,slug,acf&per_page=100`,
   },
   media: {
     GETBYID: (id: number) =>
       `${base}/media/${id}?_fields=id,title,guid,media_details`,
   },
   page: {
-    GETALL: `${base}/pages?_fields=id,title,slug,content,excerpt,status,menuitems,menu_order,acf`,
+    GETALL: `${base}/pages?_fields=id,title,slug,content,excerpt,status,menuitems,menu_order,acf&per_page=100`,
+    GETBYID: (id: number) =>
+      `${base}/pages/${id}?_fields=id,title,slug,content,excerpt,status,menuitems,menu_order,acf`,
+    GETBYSLUG: (slug: string) =>
+      `${base}/slug/${slug}?_fields=id,title,slug,content,excerpt,status,menuitems,menu_order,acf`,
   },
   slider: {
-    GETALL: `${base}/sliders?_fields=id,title,featured_media,status,acf`,
+    GETALL: `${base}/sliders?_fields=id,title,featured_media,status,acf&per_page=100`,
   },
 };
