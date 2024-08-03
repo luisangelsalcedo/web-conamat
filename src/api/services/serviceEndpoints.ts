e95d1da1ef55c370134c469f4bc3bf5aaed763e9
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2024-08-03 00:35:53 -0500

    refactor: Create get endpoint function

--- src/api/services/serviceEndpoints.ts
+++ src/api/services/serviceEndpoints.ts
@@ -2,30 +2,55 @@ import { config } from '@/config';
 
 const base = config.apiUrl;
 
-export const endpoints = {
-  blogItem: {
-    GETALL: (page: number) =>
-      `${base}/posts?page=${page}&_fields=id,slug,title,content,excerpt,featured_media,status,categories,acf&per_page=20`,
-    GETBYID: (id: number) =>
-      `${base}/posts/${id}?_fields=id,slug,title,content,excerpt,featured_media,status,categories,acf`,
-    GETBYSLUG: (slug: string) =>
-      `${base}/slug/${slug}?_fields=id,slug,title,content,excerpt,featured_media,status,categories,acf`,
+interface GetEndpoint {
+  (
+    key: 'menuitems' | 'sliders' | 'posts' | 'pages' | 'media',
+    config: { id?: number; slug?: string; page?: number }
+  ): string;
+}
+
+const params = {
+  menuitems: {
+    key: 'menuitems',
+    field: '_fields=id,slug,status,acf,name',
+    limit: 'per_page=100',
   },
-  menuItems: {
-    GETALL: `${base}/menuitems?_fields=id,name,slug,acf&per_page=100`,
+  sliders: {
+    key: 'sliders',
+    field: '_fields=id,slug,status,acf,title,featured_media',
+    limit: 'per_page=100',
+  },
+  posts: {
+    key: 'posts',
+    field:
+      '_fields=id,slug,status,acf,title,content,excerpt,featured_media,categories',
+    limit: 'per_page=20',
+  },
+  pages: {
+    key: 'pages',
+    field:
+      '_fields=id,slug,status,acf,title,content,excerpt,menuitems,menu_order',
+    limit: 'per_page=100',
   },
   media: {
-    GETBYID: (id: number) =>
-      `${base}/media/${id}?_fields=id,title,guid,media_details`,
+    key: 'media',
+    field: '_fields=id,slug,status,acf,title,guid,media_details',
+    limit: 'per_page=100',
   },
-  page: {
-    GETALL: `${base}/pages?_fields=id,title,slug,content,excerpt,status,menuitems,menu_order,acf&per_page=100`,
-    GETBYID: (id: number) =>
-      `${base}/pages/${id}?_fields=id,title,slug,content,excerpt,status,menuitems,menu_order,acf`,
-    GETBYSLUG: (slug: string) =>
-      `${base}/slug/${slug}?_fields=id,title,slug,content,excerpt,status,menuitems,menu_order,acf`,
-  },
-  slider: {
-    GETALL: `${base}/sliders?_fields=id,title,featured_media,status,acf&per_page=100`,
-  },
+};
+
+export const getEndpoint: GetEndpoint = (key, config = {}): string => {
+  const { id, slug, page } = config;
+
+  if (Object.keys(params).some(item => key === item)) {
+    let endpoint = `${base}/${key}`;
+
+    endpoint = id ? `${endpoint}/${id}` : endpoint;
+    endpoint = `${endpoint}?${params[key].field}&${params[key].limit}`;
+    endpoint = slug ? `${endpoint}&slug=${slug}` : endpoint;
+    endpoint = page ? `${endpoint}&page=${page}` : endpoint;
+
+    return endpoint;
+  }
+  throw new Error('Endpoint no found');
 };
