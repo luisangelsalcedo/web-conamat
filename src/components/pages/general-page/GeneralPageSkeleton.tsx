e0badc25e8ed265b1cf271ec0c6d62c8099fe524
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2024-08-03 20:56:33 -0500

    fix: Add layout page to the page preloader

--- src/components/pages/general-page/GeneralPageSkeleton.tsx
+++ src/components/pages/general-page/GeneralPageSkeleton.tsx
@@ -1,20 +1,24 @@
+import { PageLayout } from '@/layouts';
 import { Skeleton } from '@mui/material';
 
 export function GenralPageSkeleton() {
   return (
-    <>
-      <Skeleton />
-      <Skeleton />
-      <Skeleton />
-      <Skeleton />
-      <Skeleton />
-      <Skeleton />
-      <Skeleton />
-      <Skeleton />
-      <Skeleton />
-      <Skeleton />
-      <Skeleton />
-      <Skeleton />
-    </>
+    <PageLayout title='Cargando...' path='/ ...'>
+      <PageLayout.Header />
+      <PageLayout.Body>
+        <Skeleton />
+        <Skeleton />
+        <Skeleton />
+        <Skeleton />
+        <Skeleton />
+        <Skeleton />
+        <Skeleton />
+        <Skeleton />
+        <Skeleton />
+        <Skeleton />
+        <Skeleton />
+        <Skeleton />
+      </PageLayout.Body>
+    </PageLayout>
   );
 }
