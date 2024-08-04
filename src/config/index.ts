e5501e244be57520fa6b0f67ec42bce5fcb44d2d
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2024-08-04 07:05:31 -0500

    fix: Add errors list

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,4 +2,8 @@ export const config = {
   baseUrl: import.meta.env.BASE_URL,
   apiUrl: 'https://www.conamat.edu.pe/api/wp-json/wp/v2',
   nofoundUrl: '/no-found-page',
+  errors: {
+    PAGENOFOUND: 'Page no found',
+    EMPTYLIST: 'Empty list',
+  },
 };
