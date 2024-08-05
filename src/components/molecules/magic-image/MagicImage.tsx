c2764dd261443efad291c6afdee84765a3b1a1c3
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2024-08-05 12:23:54 -0500

    feat: Add parameter to show the title above the image

--- src/components/molecules/magic-image/MagicImage.tsx
+++ src/components/molecules/magic-image/MagicImage.tsx
@@ -6,20 +6,23 @@ interface PropsImage {
   src?: string;
   title?: string;
   icon?: React.ReactElement | string;
-  size?: string;
   border?: boolean;
+  showTitle?: boolean;
+  loading?: boolean;
 }
 
-function Image({ src, title }: PropsImage) {
+function Image({ src, title, showTitle }: PropsImage) {
+  console.log(typeof src);
   return (
     <>
-      {src ? (
+      {src !== 'undefined' ? (
         <img src={src} alt={title} />
       ) : (
         <div className='image-default'>
           <ImageIcon />
         </div>
       )}
+      {showTitle && <h3>{title}</h3>}
     </>
   );
 }
@@ -28,17 +31,24 @@ interface Props extends PropsImage {
   to?: string;
 }
 
-export function MagicImage({ src, to, title, icon, size, border }: Props) {
+export function MagicImage({
+  src,
+  to,
+  title,
+  icon,
+  border = false,
+  showTitle = false,
+}: Props) {
   return (
     <div className={`magic-image ${!!border ? 'magic-image-border' : ''}`}>
       {!!to ? (
         <Link to={to} className='image image-link'>
           {!!icon && icon}
-          <Image src={src} title={title} />
+          <Image src={src} title={title} showTitle={showTitle} />
         </Link>
       ) : (
         <div className='image'>
-          <Image src={src} title={title} />
+          <Image src={src} title={title} showTitle={showTitle} />
         </div>
       )}
     </div>
