c3723606221cebafee17cf94bba323e542239424
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2024-09-03 09:26:22 -0500

    refactor: Update fancybox version

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Fancybox } from '@fancyapps/ui';
 import { App } from './App';
 
 import 'swiper/css';
@@ -7,10 +8,10 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import 'swiper/css/scrollbar';
 import 'swiper/css/autoplay';
-import '@fancyapps/ui/dist/fancybox.umd';
-import '@fancyapps/ui/dist/fancybox.css';
+import '@fancyapps/ui/dist/fancybox/fancybox.css';
 import '@/assets/scss/global-styles.scss';
 
+Fancybox.bind('[data-fancybox]');
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
