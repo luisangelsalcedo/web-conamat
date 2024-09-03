dba872a3245310bc65b477ec714e2f24fbff8d1b
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2024-09-03 09:30:31 -0500

    style: Add new phone numbers and adapter

--- src/components/molecules/footer/Footer.tsx
+++ src/components/molecules/footer/Footer.tsx
@@ -107,10 +107,26 @@ export function Footer() {
                   +01 480 0048
                 </a>
               </li>
-              <li>
+              <li className='phones'>
+                <WassIcon />
                 <a href='https://api.whatsapp.com/send?phone=51987839552&amp;text=Me%20gustar%C3%ADa%20obtener%20m%C3%A1s%20informaci%C3%B3n'>
-                  <WassIcon />
                   987 839 552
+                </a>
+                /
+                <a href='https://api.whatsapp.com/send?phone=51983815312&amp;text=Me%20gustar%C3%ADa%20obtener%20m%C3%A1s%20informaci%C3%B3n'>
+                  983 815 312
+                </a>
+                /
+                <a href='https://api.whatsapp.com/send?phone=51934974273&amp;text=Me%20gustar%C3%ADa%20obtener%20m%C3%A1s%20informaci%C3%B3n'>
+                  934 974 273
+                </a>
+                /
+                <a href='https://api.whatsapp.com/send?phone=51997380191&amp;text=Me%20gustar%C3%ADa%20obtener%20m%C3%A1s%20informaci%C3%B3n'>
+                  997 380 191
+                </a>
+                /
+                <a href='https://api.whatsapp.com/send?phone=51986651836&amp;text=Me%20gustar%C3%ADa%20obtener%20m%C3%A1s%20informaci%C3%B3n'>
+                  986 651 836
                 </a>
               </li>
             </ul>
